2c4db20e75e08f8366bfd437a88ee838be12f596
SM-AI -Enthusiast <85394665+SHASHWAT-MISHRA-997@users.noreply.github.com>
2024-10-16 00:39:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -188,7 +188,7 @@ def main():
         - Download the caption as a text file.
         """
     )
-    st.sidebar.header("Instructions")
+    st.sidebar.header("Instructions : ")
     st.sidebar.markdown(
         """
         **1. Upload an Image:**  
@@ -218,7 +218,7 @@ def main():
     )
 
     # Instructions for using the additional app version
-    st.header("Instructions for Another Version")
+    st.header("Instructions for Another Version : ")
     st.markdown(
         """
         If you wish to try another version of the app, you can access it by clicking the link above.  
